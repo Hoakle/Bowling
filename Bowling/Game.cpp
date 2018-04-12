c5cceaf881e426085556f0eec3cf242d99f3b346
Hoakle <n.wanhem@gmail.com>
2018-04-12 11:19:06 +0200

    Test + fonction addTuple

--- Bowling/Game.cpp
+++ Bowling/Game.cpp
@@ -19,20 +19,36 @@ Game::~Game()
 int Game::calculScore()
 {
 	int somme = 0;
-	for (unsigned int i = 0; i<s.size(); ++i) {
+	unsigned int i = 0;
+	while (i<s.size() && i<10) {
+		if (get<0>(s.at(i)) + get<1>(s.at(i)) == 10) {
+			if (get<0>(s.at(i)) == 10) {
+				if (get<0>(s.at(i + 1)) == 10) {
+					somme += get<0>(s.at(i)) + get<1>(s.at(i)) + get<0>(s.at(i + 1)) + get<0>(s.at(i + 2));
+				}
+				else {
+					somme += get<0>(s.at(i)) + get<1>(s.at(i)) + get<0>(s.at(i + 1)) + get<1>(s.at(i + 1));
+				}
+				
+			}
+			else {
+				somme += get<0>(s.at(i)) + get<1>(s.at(i)) + get<0>(s.at(i + 1)) + get<1>(s.at(i + 1));
+			}
+		}
 		somme += get<0>(s.at(i)) + get<1>(s.at(i));
-
+		i++;
 	}
 	return somme;
 }
 
 void Game::addTuple(tuple<int, int> t)
 {
-	if (s.size() < 10 && get<0>(t)+get<1>(t) > 10 && get<0>(t) < 0 && get<0>(t) > 10 && get<1>(t) < 0 && get<0>(t) > 10) {
+	if (s.size() < 10 && get<0>(t)+get<1>(t) <= 10 && get<0>(t) >= 0 && get<0>(t) <= 10 && get<1>(t) >= 0 && get<0>(t) <= 10) {
 		s.push_back(t);
 	}
-	if (s.size() == 10 && get<0>(t) < 0 && get<0>(t) > 10 && get<1>(t) < 0 && get<0>(t) > 10) {
-		s.push_back(t);
+	else if (s.size() == 10 && get<0>(t) >= 0 && get<0>(t) <= 10 && get<1>(t) >= 0 && get<0>(t) <= 10) {
+		if(get<0>(s.at(9)) + get<1>(s.at(9)) == 10)
+			s.push_back(t);
 	}
 
 	return;
--- TestBowling/unittest1.cpp
+++ TestBowling/unittest1.cpp
@@ -1,18 +1,99 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-
+#include "../Bowling/Game.h"
+#include <assert.h> 
+#include <vector>
+#include <tuple>
+#include <wchar.h>
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
-namespace TestBowling
+namespace UnitTest1
 {		
 	TEST_CLASS(UnitTest1)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(_GameInit)
 		{
 			// TODO: Ici, votre code de test
+			typedef vector<tuple<int, int>> score;
+			score tl;
+			tl.push_back(tuple<int, int>(1,1));
+			tl.push_back(tuple<int, int>(2, 2));
+			Game game = Game(tl);
+			assert(tl == game.s);
+			Game game2 = Game();
+			score t2;
+			assert(t2 == game2.s);
 		}
 
+		TEST_METHOD(CalculScore)
+		{
+			typedef vector<tuple<int, int>> score;
+			score tl;
+			tl.push_back(tuple<int, int>(1, 1));
+			tl.push_back(tuple<int, int>(2, 2));
+			Game game = Game(tl);
+			Assert::IsTrue(6 == game.calculScore());
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(1, 1));
+			Assert::IsTrue(20 == game.calculScore());
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			Assert::IsTrue(80 == game.calculScore());
+			game.s.clear();
+			game.addTuple(tuple<int, int>(2, 8));
+			game.addTuple(tuple<int, int>(5, 5));
+			game.addTuple(tuple<int, int>(5, 0));
+			Assert::IsTrue(45 == game.calculScore());
+			game.s.clear();
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(10, 10));
+			Assert::IsTrue(300 == game.calculScore());
+		}
+		TEST_METHOD(AddTuple) {
+			typedef vector<tuple<int, int>> score;
+			score tl;
+			tl.push_back(tuple<int, int>(1, 1));
+			tl.push_back(tuple<int, int>(2, 2));
+			Game game = Game(tl);
+			game.addTuple(tuple<int, int>(1, 1));
+			game.addTuple(tuple<int, int>(-2, 2));
+			Assert::IsTrue(3 == game.s.size());
+			game.addTuple(tuple<int, int>(1, 1));
+			game.addTuple(tuple<int, int>(12, 2));
+			Assert::IsTrue(4 == game.s.size());
+			game.addTuple(tuple<int, int>(1, 1));
+			game.addTuple(tuple<int, int>(2, 9));
+			Assert::IsTrue(5 == game.s.size());
+			game.addTuple(tuple<int, int>(1, 1));
+			game.addTuple(tuple<int, int>(2, 2));
+			game.addTuple(tuple<int, int>(1, 1));
+			game.addTuple(tuple<int, int>(2, 2));
+			game.addTuple(tuple<int, int>(2, 2));
+			game.addTuple(tuple<int, int>(2, 2));
+			Assert::IsTrue(10 == game.s.size());
+			game.s.pop_back();
+			game.addTuple(tuple<int, int>(10, 0));
+			game.addTuple(tuple<int, int>(2, 2));
+			Assert::IsTrue(11 == game.s.size());
+			game.s.pop_back();
+			game.s.pop_back();
+			game.addTuple(tuple<int, int>(5, 5));
+			game.addTuple(tuple<int, int>(2, 2));
+			Assert::IsTrue(11 == game.s.size());
+		}
 	};
 }
